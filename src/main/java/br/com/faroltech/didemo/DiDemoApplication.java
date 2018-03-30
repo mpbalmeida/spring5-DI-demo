1f9eb20a9cd36d26116df4d07f4f508702cd1620
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-03-30 01:08:09 -0300

    Property configuration

--- src/main/java/br/com/faroltech/didemo/DiDemoApplication.java
+++ src/main/java/br/com/faroltech/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import br.com.faroltech.didemo.controllers.ConstructorInjectedController;
 import br.com.faroltech.didemo.controllers.MyController;
 import br.com.faroltech.didemo.controllers.PropertyInjectedController;
 import br.com.faroltech.didemo.controllers.SetterInjectedController;
+import br.com.faroltech.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,8 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
     }
 }
---
+++ src/main/java/br/com/faroltech/didemo/config/PropertyConfig.java
@@ -0,0 +1,43 @@
+package br.com.faroltech.didemo.config;
+
+import br.com.faroltech.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${faroltech.username}")
+    String user;
+
+    @Value("${faroltech.password}")
+    String password;
+
+    @Value("${faroltech.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        fakeDataSource.setUser(env.getProperty("FAROLTECH.USERNAME"));
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
